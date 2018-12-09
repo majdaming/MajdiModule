d7e369ef2c426c4c38767e8f4b25690ba6f9affb
LENOVO <LENOVO@DESKTOP-M2I84JS>
2018-12-09 02:00:59 +0100

    Update service

--- src/app/majdi.module.ts
+++ src/app/majdi.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MajdiComponent } from './majdi/majdi.component';
+import { MathService } from './services/math.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [MajdiComponent],
+  providers: [MathService],
   exports: [MajdiComponent]
 })
 export class MajdiModule { }
