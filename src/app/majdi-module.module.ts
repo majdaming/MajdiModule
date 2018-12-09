740af9181e932e5a281adc708f38d581b5b05e3f
LENOVO <LENOVO@DESKTOP-M2I84JS>
2018-12-09 01:29:21 +0100

    Mise en place

---
+++ src/app/majdi-module.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MajdiComponent } from './majdi/majdi.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MajdiComponent],
+  exports: [MajdiComponent]
+})
+export class MajdiModuleModule { }
---
+++ src/app/majdi/majdi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-majdi',
+  templateUrl: './majdi.component.html',
+  styleUrls: ['./majdi.component.css']
+})
+export class MajdiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
