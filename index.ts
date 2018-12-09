e0810cfea9e95d6d6186d6a0de467410cd196c3b
LENOVO <LENOVO@DESKTOP-M2I84JS>
2018-12-09 01:58:54 +0100

    Add service

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export {MajdiModule} from './src/app/majdi.module';
-export {MajdiComponent} from './src/app/majdi/majdi.component';
+export { MajdiModule } from './src/app/majdi.module';
+export { MajdiComponent } from './src/app/majdi/majdi.component';
+export { MathService } from './src/app/services/math.service';
